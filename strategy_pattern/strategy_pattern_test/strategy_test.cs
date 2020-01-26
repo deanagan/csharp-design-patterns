1b852a3b708212948826baa8b0a3981c18685544
Dean Agan <agandfr@gmail.com>
2020-01-26 16:04:42 +1100

    Add test for member with valid coupon on regular item

--- strategy_pattern/strategy_pattern_test/strategy_test.cs
+++ strategy_pattern/strategy_pattern_test/strategy_test.cs
@@ -13,7 +13,19 @@ namespace Strategy.Test
         [Test]
         public void TestGetDiscountIfMemberBoxingDayCouponValidRegularItem_ShouldReturnMemberDiscountedPrice()
         {
-            Assert.Fail();
+            // Arrange
+            var coupon = new Mock<ICoupon>();
+            coupon.Setup(c => c.IsExpired()).Returns(false);
+            coupon.Setup(c => c.Discount()).Returns(0.05);
+
+            var product = new Mock<IProduct>();
+            product.Setup(p => p.SellingPrice()).Returns(100.00);
+            
+            // Act
+            var sut = new Member(coupon.Object);
+
+            // Assert
+            Assert.AreEqual(95.00,sut.CalculatePrice(product.Object));
         }
 
         [Test]
