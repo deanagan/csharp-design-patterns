b6206b0bffc67bd7430b0bc514aa774b4c6ebcd9
Dean Agan <agandfr@gmail.com>
2020-01-26 16:01:27 +1100

    Add user story

---
+++ strategy_pattern/strategy_pattern/Member.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace Strategy
+{
+    public class Member : ICustomer
+    {
+        private readonly ICoupon _coupon;
+
+        public Member(ICoupon coupon)
+        {
+            _coupon = coupon;
+        }
+        public bool IsMember()
+        {
+            return true;
+        }
+        public double CalculatePrice(IProduct product)
+        {
+            return 0;
+        }
+    }
+}
