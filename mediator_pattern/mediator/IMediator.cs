efaa3d20707d7c4675735528d3fc1a1fe5c2f1df
Dean Agan <agandfr@gmail.com>
2020-01-29 10:19:17 +1100

    Removed code scribbles on this file

--- mediator_pattern/mediator/IMediator.cs
+++ mediator_pattern/mediator/IMediator.cs
@@ -8,15 +8,3 @@ namespace Mediator
         void AddPurchaser(IPurchaser purchaser);
     }
 }
-
-
-// IMediator = new Mediator;
-// Purchaser p1 = new Purchaser();
-// Purchaser p2 = new Purchaser();
-
-
-
-// p1.Notify("ball", "sydney");
-//     Mediator.PurchaseAlert(p1);
-
-// px.OtherPurchaser()
