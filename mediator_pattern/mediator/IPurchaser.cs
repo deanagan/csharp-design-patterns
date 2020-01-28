0f7dbee5c0718c112085babd3c1d897b94a883b6
Dean Agan <agandfr@gmail.com>
2020-01-29 10:27:49 +1100

    Cleaned up interfaces to remove notes.

--- mediator_pattern/mediator/IPurchaser.cs
+++ mediator_pattern/mediator/IPurchaser.cs
@@ -4,10 +4,9 @@ namespace Mediator
 {
     public interface IPurchaser
     {
-        string Bought(); // called by the other purchaser
-        string Location(); // called by other purchaser
-        void Receive(IPurchaser purchaser); // called by mediator to notify what other purchaser bought.
-
-        void Complete(IPurchaser purchaser); // called by client to complete a purchase
+        string Bought();
+        string Location();
+        void Receive(IPurchaser purchaser);
+        void Complete();
     }
 }
