bed148eef5385897517c7fb5829fe6b012a748ce
Dean Agan <agandfr@gmail.com>
2020-01-30 15:25:45 +1100

    Create stubbed concrete implementation of adaptee

---
+++ adapter_pattern/adapter/SocialMediaProfileAdapter.cs
@@ -0,0 +1,17 @@
+
+
+namespace Adapter
+{
+    public class SocialMediaProfileAdapter : IGoodReadsProfile
+    {
+        public string Name()
+        {
+            return null;
+        }
+
+        public string Email()
+        {
+            return null;
+        }
+    }
+}
