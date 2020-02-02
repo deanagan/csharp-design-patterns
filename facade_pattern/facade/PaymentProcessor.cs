3ea63ad80ffb2c23802bcd7e8d7ca614ad359e58
Dean Agan <agandfr@gmail.com>
2020-02-03 05:35:21 +1100

    Fix test to pass by setting environment target to SANDBOX in payment facade.

--- facade_pattern/facade/PaymentProcessor.cs
+++ facade_pattern/facade/PaymentProcessor.cs
@@ -2,9 +2,14 @@ namespace facade
 {
     public class PaymentProcessor : IPaymentProcessor
     {
+        private readonly IEnvironment _environment;
+        public PaymentProcessor(IEnvironment environment)
+        {
+            _environment = environment;
+        }
         public void InitializePaymentGatewayInterface()
         {
-            
+            _environment.environmentVariableTarget = EnvironmentTarget.SANDBOX;
         }
         public bool SubmitPayment()
         {
