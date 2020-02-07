fc10afd2ac0123a6e920fa2bb68f8a827043e4be
Dean Agan <agandfr@gmail.com>
2020-02-08 06:11:49 +1100

    Add requirement that coupons are constructed with valid expiry dates.

--- state_pattern/state/Coupon.cs
+++ state_pattern/state/Coupon.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace State
 {
     public class Coupon : ICoupon
     {
-        public Coupon(int discount)
+        private int _discount;
+        private ICouponState _validState;
+        private ICouponState _invalidState;
+        private ICouponState _currentState;
+        private DateTime _expiry;
+        
+        public Coupon(int discount, DateTime expiry)
         {
-
+            _expiry = expiry;
+            _discount = discount;
+            // Because coupon states are internal, we will
+            // couple them with the specific states so they
+            // are highly cohesive.
+            _validState = new ValidCouponState();
+            _invalidState = new InvalidCouponState();
+            _currentState = _validState;
         }
 
         public void UpdateExpiryDate(DateTime dateTime)
@@ -16,7 +30,8 @@ namespace State
         
         public decimal FinalSellingPrice(IProduct product)
         {
-            return 0;
+            var price = product.Price;
+            return price - (price * (_discount/100.0M));
         }
     }
     
