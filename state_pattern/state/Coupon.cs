fb31468585cbe9c9f6ffcd995c29c86ae44c3962
Dean Agan <agandfr@gmail.com>
2020-02-07 21:19:57 +1100

    Inject discount to concrete implementation

--- state_pattern/state/Coupon.cs
+++ state_pattern/state/Coupon.cs
@@ -1,10 +1,17 @@
+using System;
+
 namespace State
 {
     public class Coupon : ICoupon
     {
+        public Coupon(int discount)
+        {
+
+        }
+
         public void UpdateExpiryDate(DateTime dateTime)
         {
-            
+
         }
         
         public decimal FinalSellingPrice(IProduct product)
