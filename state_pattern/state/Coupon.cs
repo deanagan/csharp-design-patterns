8fbb7a5fcf635943c191e6ee9963924420cca9df
Dean Agan <agandfr@gmail.com>
2020-02-07 21:14:50 +1100

    Create concrete context

---
+++ state_pattern/state/Coupon.cs
@@ -0,0 +1,16 @@
+namespace State
+{
+    public class Coupon : ICoupon
+    {
+        public void UpdateExpiryDate(DateTime dateTime)
+        {
+            
+        }
+        
+        public decimal FinalSellingPrice(IProduct product)
+        {
+            return 0;
+        }
+    }
+    
+}
