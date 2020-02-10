cf93ff578bc9c1314794cfed62f75a9153be706a
Dean Agan <agandfr@gmail.com>
2020-02-10 21:41:58 +1100

    Fix composite to pass test.

--- composite_pattern/composite/BookComposite.cs
+++ composite_pattern/composite/BookComposite.cs
@@ -6,16 +6,20 @@ namespace Composite
 {
     public class BookComposite : IBook
     {
-        public decimal Price {get;}
+        private List<IBook> _books;
+        public decimal Price => _books.Select(book => book.Price).Sum();
         public string Name { get; private set; }
-        
+        public int Discount {get; private set;}
         public BookComposite(int discount, string name)
         {
+            Discount = discount;
+            Name = name;
+            _books = new List<IBook>();
         }
 
         public void Add(IBook book)
         {
-            
+            _books.Add(book);
         }
     }
 }
