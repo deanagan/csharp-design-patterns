0addda8fc426874c16333ddc96350b98f74d6dec
Dean Agan <agandfr@gmail.com>
2020-02-10 21:47:29 +1100

    Add test for composite containing composites.

--- composite_pattern/composite_test/composite_test.cs
+++ composite_pattern/composite_test/composite_test.cs
@@ -44,6 +44,34 @@ namespace Composite.Tests
             
             // Assert
             compositePrice.Should().Be(95.0M);
+        }
+
+        [Test]
+        public void BookPriceDiscountedWithTopSet_WhenBooksArePurchasedUnderBookCompositeWithAnotherComposite()
+        {
+            // Arrange
+            var book1 = new Book("Effective C++", 65.0M);
+            var book2 = new Book("More Effective C++", 35.0M);
+            var bookCompositeCpp = new BookComposite(5, "Effective C++ Series");
+            bookCompositeCpp.Add(book1);
+            bookCompositeCpp.Add(book2);
+
+            var bookCompositeCSharp = new BookComposite(10, "Effective C# Series");
+            var book3 = new Book("Effective C#", 70.0M);
+            var book4 = new Book("Pro C# Coding", 30.0M);
+
+            bookCompositeCSharp.Add(book3);
+            bookCompositeCSharp.Add(book4);
+
+            var effectiveSeries = new BookComposite(15, "Effective Coding Series");
+            effectiveSeries.Add(bookCompositeCpp);
+            effectiveSeries.Add(bookCompositeCSharp);
+
+            // Act
+            var effectiveSeriesPrice = ComputeDiscount(effectiveSeries.Discount, effectiveSeries.Price);
+            
+            // Assert
+            effectiveSeriesPrice.Should().Be(170.0M);
 
         }
     }
