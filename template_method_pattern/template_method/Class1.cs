a603f965dcbd45534ff727dbac3809e0b71baa96
Dean Agan <agandfr@gmail.com>
2020-02-11 20:53:09 +1100

    Create template method project and solution

---
+++ template_method_pattern/template_method/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace template_method
+{
+    public class Class1
+    {
+    }
+}
---
+++ template_method_pattern/template_method_test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace template_method_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
