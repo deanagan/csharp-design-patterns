1031de64ec4d93bcb53fc9b6d9fc32775a767244
Dean Agan <agandfr@gmail.com>
2020-02-13 21:33:51 +1100

    Complete tests for this exercise

--- template_method_pattern/template_method_test/template_method_test.cs
+++ template_method_pattern/template_method_test/template_method_test.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using Moq;
+using FluentAssertions;
 
 namespace TemplateMethod.Test
 {
@@ -12,13 +14,29 @@ namespace TemplateMethod.Test
         [Test]
         public void TaxesApplied_WhenBuyerIsHighIncomeEarner()
         {
-            Assert.Fail();
+            // Arrange
+            var highIncomeBuyer = new HighIncomeBookPriceCalculator(5);
+            var book = Mock.Of<IBook>(book => book.Price == 100.0M);
+
+            // Act
+            var priceAfterTax = highIncomeBuyer.CalculatePrice(book);
+
+            // Assert
+            priceAfterTax.Should().Be(106.4M);
         }
 
         [Test]
         public void ZeroTaxesApplied_WhenBuyerIsLowIncomeEarner()
         {
-            Assert.Fail();
+            // Arrange
+            var lowIncomeBuyer = new LowIncomeBookPriceCalculator(5);
+            var book = Mock.Of<IBook>(book => book.Price == 100.0M);
+
+            // Act
+            var priceAfterTax = lowIncomeBuyer.CalculatePrice(book);
+
+            // Assert
+            priceAfterTax.Should().Be(95.0M);
         }
     }
 }
