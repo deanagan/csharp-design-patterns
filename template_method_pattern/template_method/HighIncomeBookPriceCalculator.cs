9b6fd12054f8666d528af858b9607b0ad262a2dc
Dean Agan <agandfr@gmail.com>
2020-02-13 21:34:15 +1100

    Implement computation for high income earner

--- template_method_pattern/template_method/HighIncomeBookPriceCalculator.cs
+++ template_method_pattern/template_method/HighIncomeBookPriceCalculator.cs
@@ -2,13 +2,19 @@ namespace TemplateMethod
 {
     public class HighIncomeBookPriceCalculator : BookPriceCalculator
     {
+        private static readonly int MTaxPercentage = 12;
+        private readonly int _discount;
+        public HighIncomeBookPriceCalculator(int discountPercentage)
+        {
+            _discount = discountPercentage;
+        }
         protected override decimal ComputeTotalPriceBeforeTax(IBook books)
         {
-            return 0.0M;
+            return books.Price - (books.Price * (_discount / 100.0M));
         }
         protected override decimal ApplyTax(decimal priceBeforeTax)
         {
-            return 0.0M;
+            return priceBeforeTax + (priceBeforeTax * (MTaxPercentage / 100.0M));
         }
     }
 }
