2c39fa606b4752dfb03c5aeb399c9d3290a7f1bb
Dean Agan <agandfr@gmail.com>
2020-02-16 17:35:11 +1100

    Create add command test

---
+++ command_pattern/command_pattern_test/command_pattern_test.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using Command;
+using System.Collections.Generic;
+
+namespace Command.Test
+{
+    public class Tests
+    {
+        private IProductList _ProductList;
+        private IProduct _CleanCodeBook;
+        private IProduct _RefactoringBook;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ProductList = Mock.Of<IProductList>
+            (
+                productList => 
+                    productList.Name == "WishList" && 
+                    productList.Products == (new List<IProduct>())
+            );
+
+            _CleanCodeBook = Mock.Of<IProduct>
+            (
+                product =>
+                    product.Name == "Clean Code" &&
+                    product.Price == 45.00M
+            );
+            
+            _RefactoringBook = Mock.Of<IProduct>
+            (
+                product =>
+                    product.Name == "Refactoring" &&
+                    product.Price == 55.00M
+            );
+            
+        }
+
+        [Test]
+        public void ItemsAdded_WhenUsingAddCommand()
+        {
+            // Arrange
+            var addCommand = new AddCommand(_ProductList);
+
+            // Act
+            addCommand.Execute(_CleanCodeBook);
+            addCommand.Execute(_RefactoringBook);
+
+            // Assert
+            _ProductList.Products.Should().NotBeNullOrEmpty()
+                                          .And
+                                          .HaveCount(2)
+                                          .And
+                                          .OnlyHaveUniqueItems()
+                                          .And
+                                          .Contain(_CleanCodeBook)
+                                          .And
+                                          .Contain(_RefactoringBook);
+        }
+
+        
+    }
+}
