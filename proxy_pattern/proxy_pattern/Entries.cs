f8d25d600ca363d5482847347abcf6fc295de086
Dean Agan <agandfr@gmail.com>
2020-02-23 18:17:25 +1100

    Implemented real entries

---
+++ proxy_pattern/proxy_pattern/Entries.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+
+namespace ProxyPattern
+{
+    public class Entries : IEntries
+    {
+        private Dictionary<int, IProductInfo> _products;
+        public Entries(Dictionary<int, IProductInfo> products)
+        {
+            _products = products;
+        }
+
+        public bool Delete(int id)
+        {
+            return _products.Remove(id);
+        }
+        public IProductInfo Get(int id)
+        {
+            if (!_products.ContainsKey(id))
+            {
+                return null;
+            }
+
+            return _products[id];
+        }
+
+    }
+}
