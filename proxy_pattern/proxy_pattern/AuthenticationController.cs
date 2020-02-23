32f7c2a2c4ac1f87c205b8f2f2a808c176660c97
Dean Agan <agandfr@gmail.com>
2020-02-23 18:16:34 +1100

    Implemented authentication controller

---
+++ proxy_pattern/proxy_pattern/AuthenticationController.cs
@@ -0,0 +1,15 @@
+namespace ProxyPattern
+{
+    public class AuthenticationController : IAuthenticationController
+    {
+        private IUser _user;
+        public AuthenticationController(IUser user)
+        {
+            _user = user;
+        }
+        public bool IsAdmin()
+        {
+            return _user.IsAdmin;
+        }
+    }
+}
