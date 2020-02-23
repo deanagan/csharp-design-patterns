7c3ed01845b8f6ad8fb73e077a8ea78804b2f83a
Dean Agan <agandfr@gmail.com>
2020-02-23 18:18:00 +1100

    Implemented protection proxy for entries.

---
+++ proxy_pattern/proxy_pattern/EntriesProxy.cs
@@ -0,0 +1,28 @@
+namespace ProxyPattern
+{
+    public class EntriesProxy : IEntries
+    {
+        private IAuthenticationController _authCtrl;
+        private IEntries _realEntries;
+        public EntriesProxy(IAuthenticationController authCtrl, IEntries realEntries)
+        {
+            _authCtrl = authCtrl;
+            _realEntries = realEntries;
+        }
+
+        public bool Delete(int id)
+        {
+            var result = false;
+            if (_authCtrl.IsAdmin())
+            {
+                result = _realEntries.Delete(id);
+            }
+            return result;
+        }
+        public IProductInfo Get(int id)
+        {
+            return _realEntries.Get(id);
+        }
+
+    }
+}
