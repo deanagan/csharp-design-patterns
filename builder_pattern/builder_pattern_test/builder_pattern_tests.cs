a3d4de824cd6bf23a6ed2e6235536f78a1b87c84
Dean Agan <agandfr@gmail.com>
2020-02-25 22:23:34 +1100

    Created test scenarios

---
+++ builder_pattern/builder_pattern_test/builder_pattern_tests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace BuilderPattern.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TotalPriceIsCorrect_WhenPurchasingSingleStrategyBuild()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void TotalPriceIsCorrect_WhenPurchasingMultipleStrategyBuild()
+        {
+            Assert.Fail();
+        }
+    }
+}
