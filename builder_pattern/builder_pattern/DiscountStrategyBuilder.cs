3dfd38260f7daab6e1d4f0ac50fc066b5c77bd72
Dean Agan <agandfr@gmail.com>
2020-02-26 21:23:28 +1100

    Changed builder to abstract from interface

---
+++ builder_pattern/builder_pattern/DiscountStrategyBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    public abstract class DiscountStrategyBuilder
+    {
+        private IDiscountStrategy _discountStrategy;
+
+        public DiscountStrategyBuilder(IDiscountStrategy discountStrategy)
+        {
+            _discountStrategy = discountStrategy;
+        }
+
+        DiscountStrategyBuilder ApplicableToSKUCodeStartingWith(string skuCodeStart)
+        {
+            _discountStrategy.SkuCodeStart = skuCodeStart;
+            return this;
+        }
+
+        DiscountStrategyBuilder WithDiscount(int discountInPercentage)
+        {
+            _discountStrategy.DiscountInPercentage = discountInPercentage;
+            return this;
+        }
+        IDiscountStrategy Build()
+        {
+            return _discountStrategy;
+        }
+
+
+    }
+}
