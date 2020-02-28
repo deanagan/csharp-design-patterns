3fe4fbc3167b78160d8f03c95db36934872ec37a
Dean Agan <agandfr@gmail.com>
2020-02-28 21:51:33 +1100

    Make product properties public

--- chain_of_responsibility_pattern/chain_of_responsibility/Product.cs
+++ chain_of_responsibility_pattern/chain_of_responsibility/Product.cs
@@ -4,7 +4,7 @@ namespace ChainOfResponsibility
 {
     public class Product
     {
-        string ProductCode { get; set; }
-        decimal Price { get; set; }
+        public string ProductCode { get; set; }
+        public decimal Price { get; set; }
     }
 }
