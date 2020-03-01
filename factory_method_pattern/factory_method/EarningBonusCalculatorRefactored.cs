aefcbe15472c667267451cff5551bfaf4f006365
Dean Agan <agandfr@gmail.com>
2020-03-01 20:39:42 +1100

    Update calculator to use updated perks producer

--- factory_method_pattern/factory_method/EarningBonusCalculatorRefactored.cs
+++ factory_method_pattern/factory_method/EarningBonusCalculatorRefactored.cs
@@ -4,11 +4,15 @@ namespace FrequentFlyers
 {
     public class EarningBonusCalculator
     {
-        public int UpdatedMiles(int currentTotalMiles, int newMilesEarned)
-        {
-            var perkProducer = new PerksProducer();
-            var perks = perkProducer.GetPerks(currentTotalMiles);
+        private PerksProducer perksProducer;
 
+        public EarningBonusCalculator(PerksProducer perksProducer)
+        {
+            this.perksProducer = perksProducer;
+        }
+        public int UpdatedMiles(int currentTotalMiles, int newMilesEarned)
+        {            
+            var perks = perksProducer.GetPerks(currentTotalMiles);
             return currentTotalMiles + newMilesEarned + Convert.ToInt32(Math.Floor(newMilesEarned * perks.EarningBonus()));
         }
     }
