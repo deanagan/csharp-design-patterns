761fd10a1df4daf9fe32f0d1109289ff4d832798
Dean Agan <agandfr@gmail.com>
2020-03-07 11:04:24 +1100

    Simplifying test

--- chain_of_responsibility_pattern/chain_of_responsibility_tests/chain_of_responsibility_test.cs
+++ chain_of_responsibility_pattern/chain_of_responsibility_tests/chain_of_responsibility_test.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using Moq;
 using System;
 using FluentAssertions;
+using System.Collections.Generic;
 
 namespace ChainOfResponsibility.Tests
 {
@@ -9,6 +10,9 @@ namespace ChainOfResponsibility.Tests
     {
         private ICreditCardHandler creditCardHandler;
         private Mock<IPaymentGateway> paymentGatewayMock;
+
+        private List<string> validStartingCardNumbers = new List<string>{ "3", "4", "5"};
+
         [SetUp]
         public void Setup()
         {
@@ -17,19 +21,23 @@ namespace ChainOfResponsibility.Tests
             creditCardHandler.SetNext(new AmexCardHandler(paymentGatewayMock.Object))
                              .SetNext(new MastercardHandler(paymentGatewayMock.Object));
         }
+        
 
+        private ICreditCard PrepareCreditCardPayment(string cardNumber)
+        {
+            paymentGatewayMock
+                .Setup(gateway => gateway
+                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()))
+		        .Returns(validStartingCardNumbers.Contains(cardNumber.Substring(0,1)));
+            return Mock.Of<ICreditCard>(creditCard => creditCard.Number == cardNumber);
+        }
 
         [Test]
         public void AmexCardHandlerUsed_WhenAmexCardSubmittedForVerification()
         {
             // Arrange
-            var amexCreditCard = Mock.Of<ICreditCard>(creditCard => creditCard.Number == "341263434");
-            paymentGatewayMock
-                .Setup(gateway => gateway
-                    .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()))
-		        .Returns((ICreditCardHandler cch, ICreditCard cc) => cc.Number.StartsWith("3") && 
-                                                                     cch.GetType() == typeof(AmexCardHandler));
-
+            var amexCreditCard = PrepareCreditCardPayment("341263434");
+            
             // Act
             var isValid = creditCardHandler.IsCreditCardValid(amexCreditCard);
 
@@ -46,12 +54,7 @@ namespace ChainOfResponsibility.Tests
         public void VisaCardHandlerUsed_WhenVisaCardSubmittedForVerification()
         {
             // Arrange
-            var visaCreditCard = Mock.Of<ICreditCard>(creditCard => creditCard.Number == "41263434");
-            paymentGatewayMock
-                .Setup(gateway => gateway
-                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()))
-		        .Returns((ICreditCardHandler cch, ICreditCard cc) => cc.Number.StartsWith("4") && 
-                                                                     cch.GetType() == typeof(VisaCardHandler));
+            var visaCreditCard = PrepareCreditCardPayment("41263434");
 
             // Act
             var isValid = creditCardHandler.IsCreditCardValid(visaCreditCard);
@@ -65,17 +68,11 @@ namespace ChainOfResponsibility.Tests
 
         }
 
-
         [Test]
         public void MasterCardHandlerUsed_WhenVisaCardSubmittedForVerification()
         {
             // Arrange
-            var masterCreditCard = Mock.Of<ICreditCard>(creditCard => creditCard.Number == "5384683");
-            paymentGatewayMock
-                .Setup(gateway => gateway
-                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()))
-		        .Returns((ICreditCardHandler cch, ICreditCard cc) => cc.Number.StartsWith("5") && 
-                                                                     cch.GetType() == typeof(MastercardHandler));
+            var masterCreditCard = PrepareCreditCardPayment("5384683");
 
             // Act
             var isValid = creditCardHandler.IsCreditCardValid(masterCreditCard);
@@ -93,22 +90,16 @@ namespace ChainOfResponsibility.Tests
         public void CardHandlerNull_WhenInvalidCardSubmittedForVerification()
         {
             // Arrange
-            var creditCard = Mock.Of<ICreditCard>(creditCard => creditCard.Number == "9244683");
-
-            paymentGatewayMock
-                .Setup(gateway => gateway
-                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()))
-		        .Returns(false);
+            var invalidCreditCard = PrepareCreditCardPayment("9244683");
 
             // Act
-            var isValid = creditCardHandler.IsCreditCardValid(creditCard);
+            var isValid = creditCardHandler.IsCreditCardValid(invalidCreditCard);
 
             // Assert
             isValid.Should().BeFalse();
             paymentGatewayMock
                 .Verify(paymentGateway => paymentGateway
-                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>())
-                                    , Times.Never);
+                .SubmitVerification(It.IsAny<ICreditCardHandler>(), It.IsAny<ICreditCard>()), Times.Never);
 
         }
     }
