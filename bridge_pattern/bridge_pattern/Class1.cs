6d674a587dc17d87ec425d0b84bd2a2995c8f71d
Dean Agan <agandfr@gmail.com>
2020-03-21 20:19:56 +1100

    Temporarily commit dummy classes for now

---
+++ bridge_pattern/bridge_pattern/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bridge_pattern
+{
+    public class Class1
+    {
+    }
+}
---
+++ bridge_pattern/bridge_pattern_test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace bridge_pattern_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
