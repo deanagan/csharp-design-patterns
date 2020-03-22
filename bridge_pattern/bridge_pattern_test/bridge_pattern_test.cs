5bb0a5f8a391d68c5cfaf20075606c1b5d87c788
Dean Agan <agandfr@gmail.com>
2020-03-22 13:33:43 +1100

    Implement tests

---
+++ bridge_pattern/bridge_pattern_test/bridge_pattern_test.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+
+namespace BridgePattern.Test
+{
+    public class Tests
+    {
+        private IPaymentGateway _mPaymentGateway;
+        [SetUp]
+        public void Setup()
+        {
+            _mPaymentGateway = Mock.Of<IPaymentGateway>();
+        }
+
+        private void VerifyPaymentMethod<T>(decimal amount)
+        {
+            Mock.Get(_mPaymentGateway).Verify(
+                pg => pg.ProcessPayment(20.0M, It.Is<CreditCardPayment>
+                    (p => p.GetType() == typeof(T))), Times.Once());
+
+        }
+
+        [Test]
+        public void CreditCardPaymentReceivedWhenCheckingOutWithCreditCard()
+        {
+            // Arrange
+            var paymentMethod = new CreditCardPayment(_mPaymentGateway);
+            var order = new Order(paymentMethod);
+            
+            // Act
+            order.Checkout(20.0M);
+
+            // Assert
+            VerifyPaymentMethod<CreditCardPayment>(20.0M);            
+        }
+
+         [Test]
+        public void PaypalPaymentReceivedWhenCheckingOutWithPaypal()
+        {
+            // Arrange
+            var paymentMethod = new PaypalPayment(_mPaymentGateway);
+            var order = new Order(paymentMethod);
+            
+            // Act
+            order.Checkout(20.0M);
+
+            // Assert
+            VerifyPaymentMethod<PaypalPayment>(20.0M);            
+        }
+    }
+}
