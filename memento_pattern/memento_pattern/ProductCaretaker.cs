df93362174807b9c0ed9ef8ee2c92d2d4bcfb4c8
Dean Agan <agandfr@gmail.com>
2020-03-26 22:25:20 +1100

    Implement skeletal class to compile

---
+++ memento_pattern/memento_pattern/ProductCaretaker.cs
@@ -0,0 +1,15 @@
+namespace MementoPattern
+{
+    public class ProductCaretaker : IProductCaretaker
+    {
+        public void AddProductMemento(IProduct product)
+        {
+
+        }
+
+        public IProduct GetLastMemento()
+        {
+            return null;
+        }
+    }
+}
---
+++ memento_pattern/memento_pattern/ProductOriginator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MementoPattern
+{
+    public class ProductOriginator : IProductOriginator
+    {
+        private IProduct product;
+
+        public ProductOriginator(IProduct product)
+        {
+            SetMemento(product);
+        }
+
+        public void SetMemento(IProduct product)
+        {
+            this.product = product;
+        }
+
+        public IProduct CreateMemento()
+        {
+            return this.product;
+        }
+    }
+}
