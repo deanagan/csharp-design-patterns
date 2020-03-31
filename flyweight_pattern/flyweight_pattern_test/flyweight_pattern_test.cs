802dc305586c11b1c416d96781a44954f93d3e3e
Dean Agan <agandfr@gmail.com>
2020-03-31 19:40:17 +1100

    Rename test

---
+++ flyweight_pattern/flyweight_pattern_test/flyweight_pattern_test.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FlyweightPattern.Test
+{
+    public class FlyweightPatternTest
+    {
+        [Fact]
+        public void ObjectNotRecreated_WhenAskingSameObject()
+        {
+
+        }
+    }
+}
