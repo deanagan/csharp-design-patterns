13b99ac4917ecbbcb2da0a407211ad3c7e907ef4
Dean Agan <agandfr@gmail.com>
2020-04-01 19:53:33 +1100

    Implemented for test to not be recreated

--- flyweight_pattern/flyweight_pattern_test/flyweight_pattern_test.cs
+++ flyweight_pattern/flyweight_pattern_test/flyweight_pattern_test.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using Moq;
+using FluentAssertions;
+
 
 namespace FlyweightPattern.Test
 {
     public class FlyweightPatternTest
     {
+        private TextDownloader textDownloader;
+        public FlyweightPatternTest()
+        {
+            this.textDownloader = Mock.Of<TextDownloader>();
+        }
         [Fact]
         public void ObjectNotRecreated_WhenAskingSameObject()
         {
+            // Arrange
+            var sut = new XmlTextBlobFactory(this.textDownloader);
 
+            // Act
+            var blob1 = sut.GetTextBlob(1);
+            var blob2 = sut.GetTextBlob(1);
+
+            // Assert
+            blob1.GetHashCode().Should().Be(blob2.GetHashCode());
         }
     }
 }
