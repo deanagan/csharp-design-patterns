f93ecca152e7425f34b8fd959113a7b2c48146ab
Dean Agan <agandfr@gmail.com>
2020-04-26 19:17:26 +1000

    Remove old ones. Will rename and use xunit.

--- abstract_factory_pattern/abstract_factory/IProcessor.cs
+++
@@ -1,9 +0,0 @@
-namespace Laptop
-{
-    //TODO: Use Generic Constraints
-    public interface IProcessor
-    {
-        string BrandName();
-        double SpeedInGigaHertz();
-    }
-}
--- abstract_factory_pattern/abstract_factory/IStorage.cs
+++
@@ -1,9 +0,0 @@
-namespace Laptop
-{
-    //TODO: Use Generic Constraints
-    public interface IStorage
-    {
-        string HardwareType();
-        int ReadSpeedInMBytesPerSec();
-    }
-}
