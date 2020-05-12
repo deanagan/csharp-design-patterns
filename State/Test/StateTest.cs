854ca2590d4d4a73fb62a58c6bc4ada58114f86a
Dean Agan <agandfr@gmail.com>
2020-05-12 17:33:02 +1000

    Updated tests

--- State/Test/StateTest.cs
+++ State/Test/StateTest.cs
@@ -1,55 +1,45 @@
 using System;
-using NUnit.Framework;
+using Xunit;
 using Moq;
 using FluentAssertions;
-using State;
 
 namespace State.Test
 {
-    public class Tests
+    public class StateShould
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void ProductPrice_IsDiscounted_WhenCoupon_IsValid()
+        private readonly IProduct _product = Mock.Of<IProduct> ( p => p.Price == 100.0M);
+        [Fact]
+        public void ReturnDiscountedProductPrice_WhenCoupon_IsValid()
         {
             // Arrange
-            var product = Mock.Of<IProduct> ( p => p.Price == 100.0M);
-            var expiry = DateTime.Today.AddDays(2);
-            var coupon = new Coupon(5, expiry);       
-            
-            // Assert
-            coupon.ApplyDiscountTo(product).Should().Be(95.0M);
-        }
-
-        [Test]
-        public void ProductPrice_IsSame_WhenCoupon_IsExpired()
-        {
-            // Arrange
-            var product = Mock.Of<IProduct> ( p => p.Price == 100.0M);
             var expiry = DateTime.Today.AddDays(2);
             var coupon = new Coupon(5, expiry);
-
             // Act
-            coupon.UpdateExpiryDate(expiry.AddDays(-4));
-
+            var price = coupon.ApplyDiscountTo(_product);
             // Assert
-            coupon.ApplyDiscountTo(product).Should().Be(100.0M);
+            price.Should().Be(95.0M);
         }
 
-        [Test]
-        public void ArgumentException_IsThrown_WhenCouponConstructedAsExpired()
+        [Fact]
+        public void ReturnSameProductPrice_WhenCouponIsExpired()
         {
             // Arrange
-            var product = Mock.Of<IProduct> ( p => p.Price == 100.0M);
+            var expiry = DateTime.Today.AddDays(2);
+            var coupon = new Coupon(5, expiry);
+            coupon.UpdateExpiryDate(expiry.AddDays(-4));
+            // Act
+            var price = coupon.ApplyDiscountTo(_product);
+            // Assert
+            price.Should().Be(100.0M);
+        }
+
+        [Fact]
+        public void ThrowArgumentException_WhenCouponConstructedAsExpired()
+        {
+            // Arrange
             var expiry = DateTime.Today.AddDays(-2);
-            
             // Act
             Action act = () => new Coupon(5, expiry);
-
             // Assert
             act.Should().ThrowExactly<ArgumentException>();
         }
