2f4fe33874f801f6d57f8c56eba47bf978098656
Dean Agan <agandfr@gmail.com>
2020-05-14 18:16:17 +1000

    Migrate tests to xunit

--- TemplateMethod/Test/TemplateMethodTest.cs
+++ TemplateMethod/Test/TemplateMethodTest.cs
@@ -1,18 +1,13 @@
-using NUnit.Framework;
+using Xunit;
 using Moq;
 using FluentAssertions;
 
 namespace TemplateMethod.Test
 {
-    public class Tests
+    public class TemplateMethodShould
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void TaxesApplied_WhenBuyerIsHighIncomeEarner()
+        [Fact]
+        public void ApplyTaxes_WhenBuyerIsHighIncomeEarner()
         {
             // Arrange
             var highIncomeBuyer = new HighIncomeBookPriceCalculator(5);
@@ -25,8 +20,8 @@ namespace TemplateMethod.Test
             priceAfterTax.Should().Be(106.4M);
         }
 
-        [Test]
-        public void ZeroTaxesApplied_WhenBuyerIsLowIncomeEarner()
+        [Fact]
+        public void NotApplyTaxes_WhenBuyerIsLowIncomeEarner()
         {
             // Arrange
             var lowIncomeBuyer = new LowIncomeBookPriceCalculator(5);
