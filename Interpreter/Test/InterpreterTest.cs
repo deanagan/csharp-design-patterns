50a9bbb376c41f6cde2bd1ff6a86fc6195e90116
Dean Agan <agandfr@gmail.com>
2020-05-17 15:54:27 +1000

    Add failing test

--- Interpreter/Test/InterpreterTest.cs
+++ Interpreter/Test/InterpreterTest.cs
@@ -1,14 +1,23 @@
-using System;
+using FluentAssertions;
 using Xunit;
 
-namespace Test
+namespace Interpreter.Test
 {
-    public class UnitTest1
+    public class InterpreterShould
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(new object[] {"VI", 6})]
+        // [InlineData(new object[] {"LXXVI", 76})]
+        // [InlineData(new object[] {"CDXCIX", 499})]
+        // [InlineData(new object[] {"MMMDCCCLXXXVIII", 3888})]
+        public void ReturnNumericValue_WhenGivenRomanNumerals(string romanNumerals, int expectedValue)
         {
-
+            // Arrange
+            var interpreter = new Interpreter();
+            // Act
+            var numericValue = interpreter.Interpret(romanNumerals);
+            // Assert
+            numericValue.Should().Be(numericValue);
         }
     }
 }
