bcc3893808cdd19f8c6c6cb00ab5be259eb99ff6
Dean Agan <agandfr@gmail.com>
2020-05-17 15:53:26 +1000

    Skeletal code

---
+++ Interpreter/Source/RomanNumeral.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interpreter
+{
+    public class Interpreter
+    {
+        public int Interpret(string romanNumerals)
+        {
+            return 0;
+        }
+    }
+}
