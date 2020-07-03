dbc28410c5831cc4e5e8b800ca6f744294d69aee
Dean Agan <agandfr@gmail.com>
2020-07-03 18:29:44 +1000

    Removed interpreter solution

--- Interpreter/Source/IExpression.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Interpreter
-{
-    public interface IExpression
-    {
-        int Interpret(IProduct product);
-    }
-}
--- Interpreter/Source/IProduct.cs
+++
@@ -1,9 +0,0 @@
-namespace Interpreter
-{
-    public interface IProduct
-    {
-        bool WillExpire { get; set; }
-        string Name { get; set; }
-        decimal Price { get; set; }
-    }
-}
